dd6faa74b8feb4ec9810e21e4850c38e25a8c043
artiak <arturpetrovich@gmail.com>
2024-04-28 22:26:25 +0300

    Rename param from 'in_date' to 'date'

--- task_01.py
+++ task_01.py
@@ -7,15 +7,15 @@ def parse_date(in_date: str):
     except Exception:
         print("Invalid date format. Please enter a date in YYYY-MM-DD format")
 
-def get_days_from_today(date_str: str):
-    in_date = parse_date(date_str)
+def get_days_from_today(date: str):
+    parsed_date = parse_date(date)
     
-    if not in_date:
+    if not parsed_date:
         return
     
     cur_date = datetime.today()
     
-    return (cur_date - in_date).days
+    return (cur_date - parsed_date).days
 
 # testing
 while True:
