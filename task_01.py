4d77d1cb6c2f62f42f2f5679d6d7d2beeda55f2f
artiak <arturpetrovich@gmail.com>
2024-04-29 21:01:11 +0300

    Move parse_date() to testing section

--- task_01.py
+++ task_01.py
@@ -1,5 +1,18 @@
 from datetime import datetime
 
+
+def get_days_from_today(date: str):
+    parsed_date = parse_date(date)
+
+    if not parsed_date:
+        return
+
+    cur_date = datetime.today()
+
+    return (cur_date - parsed_date).days
+
+# testing
+
 def parse_date(in_date: str):
     date_format = '%Y-%m-%d'
     try:
@@ -7,17 +20,7 @@ def parse_date(in_date: str):
     except Exception:
         print("Invalid date format. Please enter a date in YYYY-MM-DD format")
 
-def get_days_from_today(date: str):
-    parsed_date = parse_date(date)
-    
-    if not parsed_date:
-        return
-    
-    cur_date = datetime.today()
-    
-    return (cur_date - parsed_date).days
 
-# testing
 while True:
     date_str = input("Enter a date (YYYY-MM-DD): ")
-    print(f"Days difference: {get_days_from_today(date_str)}") 
+    print(f"Days difference: {get_days_from_today(date_str)}")
